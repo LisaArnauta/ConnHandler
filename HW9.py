f9643d8dfadade63d6d17f5550e237cd13893c17
Lisa <67687558+LisaArnauta@users.noreply.github.com>
2020-07-23 16:14:07 +0300

    Add files via upload

---
+++ HW9.py
@@ -0,0 +1,49 @@
+class ConnHandler:
+    __slots__ = ['_unit_name', '_mac_address', '_ip_address', '_login', '_password']
+
+    def __init__(self, unit_name = '', mac_address='', ip_address='', login='', password=''):
+        self._unit_name = unit_name
+        self._mac_address = mac_address
+        self._ip_address= ip_address
+        self._login = login
+        self._password = password
+
+    @property
+    def unit_name(self):
+        return self._unit_name
+
+    @unit_name.setter
+    def unit_name(self, new_name):
+        self._unit_name = new_name
+
+    @property
+    def mac_address(self):
+        return self._mac_address
+
+    @mac_address.setter
+    def mac_address(self, new_mac_address):
+        self._mac_address = new_mac_address
+
+    @property
+    def ip_address(self):
+        return self._ip_address
+
+    @ip_address.setter
+    def ip_address(self, new_ip_address):
+        self._ip_address = new_ip_address
+
+    @property
+    def login(self):
+        return self._login
+
+    @login.setter
+    def login(self, new_login):
+        self._login = new_login
+
+    @property
+    def password(self):
+        return self._password
+
+    @password.setter
+    def passsword(self, new_password):
+        self._password = new_password
